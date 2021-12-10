91efa173fd2ac944a71427ebc369e71191f90e36
VladSemiletov <semiletoffvladislav@yandex.ru>
2021-12-10 18:14:32 +0300

    first commit

---
+++ ordersapp/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+
+from ordersapp.models import Order, OrderItem
+
+
+class OrderForm(forms.ModelForm):
+    model = Order
+
+    class Meta:
+        model = Order
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+
+class OrderItemForm(forms.ModelForm):
+    price = forms.CharField(label='Цена (руб/шт)', required=False)
+
+    class Meta:
+        model = OrderItem
+        exclude = ()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+
